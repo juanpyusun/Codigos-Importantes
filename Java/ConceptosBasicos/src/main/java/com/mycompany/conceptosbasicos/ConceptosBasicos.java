e8931b80e047f5764213883bb4168a05edc6b7e9
Juan Yusunguaira <103352879+juanpyusun@users.noreply.github.com>
2023-02-09 21:12:05 -0500

    Add files via upload

---
+++ Java/ConceptosBasicos/src/main/java/com/mycompany/conceptosbasicos/ConceptosBasicos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conceptosbasicos;
+
+/**
+ *
+ * @author SENA
+ */
+
+/*
+Comentarios de varias lineas
+*/
+
+//Comentario de una linea
+public class ConceptosBasicos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("2023-02-09");
+        System.out.println("ADSO-6");
+    }
+}
