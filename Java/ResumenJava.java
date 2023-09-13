9d4ea6942fb25e9950e0f33ac4270f872f246038
Juan Yusunguaira <103352879+juanpyusun@users.noreply.github.com>
2023-09-13 12:56:55 -0500

    Update ResumenJava.java

--- Java/ResumenJava.java
+++ Java/ResumenJava.java
@@ -141,9 +141,90 @@ public class HelloWorld{
         variableTipoDecimal = Double.parseDouble(JOptionPane.showInputDialog("Ingrese un decimal"));
         //******************************************************************************
         //CONDICIONALES
-        
+
+       //1.se puede usar solo el if
+        if (condicion){
+            //En este bloque va todo el codigo si condicion es verdadera
+        }
+
+        //2.se puede usar tambien el if-else, si la condicion es verdadera entra al if, si es falsa entra al bloque del else
+        if (condicion){
+            //En este bloque va todo el codigo si condicion es verdadera
+        }
+        else{
+            //En este bloque va todo el codigo si condicion es falsa
+        }
+
+        //3.Tambien podemos usar if-else if-else, si la condicion es falsa, va al else if y vuelve a preguntar, y asi sucesivamente sigue  preguntando en varios else if, si no cumple en ninguno entra al else
+        if (condicion1){
+            //En este bloque va todo el codigo si condicion1 es verdadera
+        }
+        else if (condicion2){
+            //En este bloque va todo el codigo si condicion2 es verdadera
+        }
+        else if (condicion3){
+            //En este bloque va todo el codigo si condicion3 es verdadera
+        }
+        else if (condicion4){
+            //En este bloque va todo el codigo si condicion4 es verdadera
+        }
+        else if(condicion5){
+            //En este bloque va todo el codigo si condicion5 es verdadera
+        }
+        else{
+            //En este bloque va todo el codigo si ninguna de las anteriores condiciones es verdadera
+        }
+
+        //4.por otro lado tenemos el if de una linea o tambien llamado operador ternario
+        String mensaje;
+        boolean condicion = true;
+
+        mensaje = (condicion) ? "aqui va el mensaje o valor numerico si la condicion es verdadera" : "aqui va el mensaje o valor numerico si la condicion es falsa";
+
+        //5.Por otro lado tenemos la estructura switch
+
+        char letra = 'a';
+
+        switch(letra){
+            case 'a':
+                System.out.println("primera letra del abecedario");
+            break;
+
+            case 'b':
+                System.out.println("segunda letra del abecedario");
+            break;
+
+            case 'c':
+                System.out.println("tercera letra del abecedario");
+            break;
+
+            default:
+                System.out.println("Ud no escogio ninguna letra del abecedario");
+            break;
+        }        
         //******************************************************************************
         //CICLOS
+         /*
+            contador: variable que va aumentando de uno en uno por ejemplo:  x++ que es lo mismo que tener x = x + 1    lo cual es exactamente lo mismo que x += 1
+            acumulador: variable que va aumentando de acuerdo a otra variable  por ejemplo: total += y 
+            observador: una variable que toma un valor dependiendo de la condicion
+            break: manera de obligar un cicloa finalizar obigatoriamente
+            continue: manera de obligar un ciclo a omitir la iteracion actual
+        */
+        //while
+        while(condicion){
+           //aqui se va a ejecutar todo el codigo necesario mientras la condicion sea verdadera
+        }
+
+        //do-while
+        do{
+           //aqui se va a ejecutar todo el codigo necesario mientras la condicion sea verdadera
+        }while(condicion);
+
+        //for
+        for(int i=valorInicial; i<valorFinal; i++){
+           //aqui va el codigo que se repetira las veces que sea necesario
+        }
         
         //******************************************************************************
         //ARREGLOS
